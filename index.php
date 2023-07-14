763c295578ae0ad60f234fa157028f3b560cbbd3
Shams Pathan <shams@bitmascot.com>
2023-07-14 15:55:30 +0600

    Simple route added

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 require 'vendor/autoload.php';
 
+// simple route
+$path = (string)trim(filter_var($_SERVER["REQUEST_URI"], FILTER_SANITIZE_URL));
+
+switch ($path) {
+    case "/login":
+        include("src/view/login.html");
+        die();
+    case "/register":
+        include("src/view/register.html");
+        die();
+}
+
 use Shams\Simple\middleware\AuthMiddleware;
 
 $middleware = new AuthMiddleware();
@@ -8,5 +20,5 @@ $middleware = new AuthMiddleware();
 if ($middleware->authCheck()) {
     include("src/view/home.html");
 } else {
-    include("src/view/login.html");
+    include("src/view/register.html");
 }
