ae55ee93b21a890e1c2bf85805a9ea6ae25d3bc2
Shams Pathan <shams@bitmascot.com>
2023-08-15 12:48:35 +0600

    route moved inside middleware

--- index.php
+++ index.php
@@ -5,28 +5,40 @@ use Shams\Simple\controller\AuthController;
 // simple route
 $path = (string)trim(filter_var($_SERVER["REQUEST_URI"], FILTER_SANITIZE_URL));
 
-switch ($path) {
-    case "/login":
-        include("src/view/login.html");
-        die();
-        
-    case "/register":
-        include("src/view/register.html");
-        die();
-
-    case "/logout":
-        $auth = new AuthController();
-        $auth->logout();
-        return header('Location: /');
-    
-}
-
 use Shams\Simple\middleware\AuthMiddleware;
 
 $middleware = new AuthMiddleware();
 
 if ($middleware->authCheck()) {
-    include("src/view/home.html");
+    
+    switch ($path) {
+        case "/":
+            include("src/view/home.html");
+            die();
+        case "/about":
+            include("src/view/about.html");
+            die();
+        case "/logout":
+            $auth = new AuthController();
+            $auth->logout();
+            return header('Location: /');
+        
+    }
 } else {
-    include("src/view/login.html");
+    switch ($path) {
+        case "/":
+        case "/login":
+            include("src/view/login.html");
+            die();
+            
+        case "/register":
+            include("src/view/register.html");
+            die();
+    
+        case "/logout":
+            $auth = new AuthController();
+            $auth->logout();
+            return header('Location: /');
+        
+    }
 }
